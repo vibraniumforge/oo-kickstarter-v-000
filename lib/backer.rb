a37ebcb45f412f94ebcafc737457f1f6e9954863
Greg Morgan <gregm9999@gmail.com>
2018-11-12 22:36:02 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,10 @@
 class Backer
   attr_accessor :backed_projects, :name
-  @backed_projects=[]
+
 
   def initialize(name)
     @name=name
+    @backed_projects=[]
   end
 
   def back_project(project)
